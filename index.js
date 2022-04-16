70540b1a98f447eb28e007c1c07e3adf5b0f91ec
YuraProgStan <yurstan@gmail.com>
2022-04-16 18:34:48 +0300

    full

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+
+import path from 'path';
+// import {requestTime, logger} from './middlewares.js';
+import serverRoutes from './routes/servers.js'
+
+const __dirname = path.resolve();
+const PORT = process.env.PORT ?? 5100;
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname,'ejs'));
+console.log(app.get('views'));
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+// app.use(requestTime);
+// app.use(logger);
+app.use(serverRoutes);
+app.get("/", (req, res) => {
+    res.render('index',{title: 'Main Page'})
+})
+
+app.get("/features", (req, res) => {
+    res.render('features', {title: "Features"})
+})
+// app.get('/', (req, res)=>{
+//     res.sendFile(path.resolve(__dirname, 'static', 'index.html'));
+// })
+
+// app.get('/features', (req, res)=>{
+//     res.sendFile(path.resolve(__dirname, 'static', 'features.html'));
+// })
+
+// app.get('/download', (req, res)=>{
+//     console.log(req.requestTime);
+//     res.download(path.resolve(__dirname, 'static', 'index.html'));
+// })
+
+app.listen(PORT, ()=>{
+    console.log(`Server has been started on ${PORT}...`)
+})
